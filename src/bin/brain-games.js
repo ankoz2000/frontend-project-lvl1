5b37699c4ba6efad85e6aada440685cdedfbc403
AndrewKN <ankoz2000@mail.ru>
2019-07-27 19:46:53 +0300

    Conversation with user: done!

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
+import { getUserName } from '../index';
 console.log("Welcome to the brain-games!");
+console.log("What is your name?");
+const name = getUserName();
+console.log(`Hello, ${name}!`);
