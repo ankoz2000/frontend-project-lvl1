5de199c0bee1181a76aebcb3c3291719a65c57a0
AndrewKN <ankoz2000@mail.ru>
2019-09-10 16:47:35 +0300

    delete wrong conditions

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,12 +3,12 @@ import { getRandomInt, gameProcess } from '..';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
-  const moduledNumber = Math.abs(number);
-  if (moduledNumber % 2 === 0 || moduledNumber < 2) return moduledNumber === 2;
-  const k = Math.round(Math.sqrt(moduledNumber));
-  if (moduledNumber === 3) return true;
+  if (number < 0) return false;
+  if (number < 2 || number % 2 === 0) return number === 2;
+  const k = Math.round(Math.sqrt(number));
+  if (number === 3) return true;
   for (let i = 3; i <= k + 1; i += 2) { // Check uneven divider
-    if (moduledNumber % i === 0) return false;
+    if (number % i === 0) return false;
   }
   return true;
 };
