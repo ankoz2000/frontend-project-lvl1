8e82dbd89e4e1adc89ad2dfd1f7388e7fc0f48e8
AndrewKN <ankoz2000@mail.ru>
2019-09-15 20:04:00 +0300

    delete unnecessary comments

--- src/index.js
+++ src/index.js
@@ -12,11 +12,6 @@ const getUserName = () => {
   return userName;
 };
 
-/* export const getUserAnswer = () => {
-  const answer = readlineSync.question('Your answer: ');
-  return answer;
-}; */
-
 export const getRandomInt = (min, max) => {
   const number = Math.floor(Math.random() * (max - min + 1)) + min;
   return number;
@@ -52,63 +47,3 @@ export const gameProcess = (gameDescription, gameQuestion) => {
   congratulation(userName);
   return userName;
 };
-
-/* const isShowMessage = (flag) => {
-  if (flag !== 1 && flag !== 3) return true;
-  return false;
-}; */
-
-/* const showCongratulation = (flag, userName) => {
-  switch (flag) {
-    case 0: {
-      congratulation(userName);
-      break;
-    }
-    case 1: break;
-    case 2: break;
-    default: {
-      congratulation(userName);
-    }
-  }
-}; */
-
-/*
-export const allGameProcess = (arrayOfGames) => {
-  let userName = '';
-  let flag = 2; // if flag = 2 user see welcome and meeting messages, but not congratulations;
-  for (let i = 0; i < gamesCount; i += 1) {
-    console.log(`${flag}`);
-    if (i === gamesCount - 1) flag = 3; // if flag = 3 user see only congratulations;
-    /*   take user's name and
-    save it in a variable (line 88)
-    if (flag === 2) {
-      userName = arrayOfGames[i](flag, userName);
-      flag = 1; // user doesn't see anything;
-    } else {
-      arrayOfGames[i](flag, userName);
-    }
-    console.log(`${flag}`);
-  }
-  return userName;
-};
-*/
-/* export const gameProcess = (gameRules, flag, userName, gameQuestion) => {
-  let tmp = 0;
-  let localUserName = userName;
-  if (isShowMessage(flag)) {
-    welcome();
-  }
-  console.log(`${gameRules}`);
-  console.log('');
-  if (isShowMessage(flag)) {
-    localUserName = getUserName();
-  }
-  do {
-    const rightAnswer = gameQuestion(localUserName);
-    const answer = readlineSync.question('Your answer: ');
-    const check = isAnswerRight(answer, rightAnswer);
-    tmp += playRounds(check, answer, rightAnswer, localUserName);
-  } while (tmp !== rightAnswersCount);
-  showCongratulation(flag, localUserName);
-  return localUserName;
-}; */
